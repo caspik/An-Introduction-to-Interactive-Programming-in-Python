2aeb1da2a757468590740b2f22a66d50e7950b89
Dmitriy <caspik@users.noreply.github.com>
2013-10-17 14:18:08 -0600

    Create mini-project-2.py

---
+++ mini-project-2.py
@@ -0,0 +1,69 @@
+# helper functions
+import random 
+
+def number_to_name(number):
+    # fill in your code below
+    if number == 0:
+        return "rock"
+    elif number == 1:
+        return "Spock"
+    elif number == 2:
+        return "paper"
+    elif number == 3:
+        return "lizard"
+    elif number == 4:
+        return "scissors"
+    else:
+        return "Wrong choise"
+    # convert number to a name using if/elif/else
+    # don't forget to return the result!
+
+    
+def name_to_number(name):
+    # fill in your code below
+    if name == "rock":
+        return 0
+    elif name == "Spock":
+        return 1
+    elif name == "paper":
+        return 2
+    elif name == "lizard":
+        return 3
+    elif name == "scissors":
+        return 4
+    else:
+        return "Wrong choise"
+    # convert name to number using if/elif/else
+    # don't forget to return the result!
+
+
+def rpsls(name): 
+    # fill in your code below
+
+    # convert name to player_number using name_to_number
+    player_number = name_to_number(name)
+    # compute random guess for comp_number using random.randrange()
+    comp_number = random.randrange(0,5)
+    # compute difference of player_number and comp_number modulo five
+    difference = (player_number - comp_number) % 5
+    # use if/elif/else to determine winner
+    if difference == 1 or difference == 2:
+        winner = 'Player'
+    if difference == 3 or difference == 4:
+        winner = 'Computer'
+    # convert comp_number to name using number_to_name
+    
+    # print results
+    print "Player chooses", number_to_name(player_number)
+    print "Computer chooses", number_to_name(comp_number) 
+    if difference:   
+        print winner, "wins!\n"
+    else:
+        print "Player and computer tie!\n"
+    
+# test your code
+rpsls("rock")
+rpsls("Spock")
+rpsls("paper")
+rpsls("lizard")
+rpsls("scissors")
