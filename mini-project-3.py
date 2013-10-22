f00459da160b8fc1a8a4a32ef0f8b4639f99b731
Dmitriy <caspik@users.noreply.github.com>
2013-10-22 09:47:40 -0600

    Update mini-project-3.py
    added frame, control field.

--- mini-project-3.py
+++ mini-project-3.py
@@ -2,10 +2,12 @@
 # input will come from buttons and an input field
 # all output for the game will be printed in the console
 
-
+import simplegui
+import random
+import math
 
 # initialize global variables used in your code
-
+num_range = 100
 
 
 # helper function to start and restart the game
@@ -36,14 +38,17 @@ def input_guess(guess):
     
 # create frame
 
+f = simplegui.create_frame("Guess the number!",200,200)
 
-
+f.add_button("Range is [0, 100]", range100, 200)
+f.add_button("Range is [0, 1000]", range1000, 200)
+f.add_input("Input your guess", input_guess, 200)
 # register event handlers for control elements
 
 
 
 # call new_game and start frame
 
-
+new_game()
 
 # always remember to check your completed program against the grading rubric
